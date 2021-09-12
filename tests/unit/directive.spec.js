cd29b82a3748287f0108c525f13082448283dc46
Dipak Sarkar <sdepokbd@gmail.com>
2021-09-12 13:01:00 +0530

    updated test script

--- tests/unit/directive.spec.js
+++ tests/unit/directive.spec.js
@@ -1,5 +1,5 @@
 import directive from '../../src/directive'
 
 test ('should not throw error on empty config', () => {
-  expect(() => directive({}, {})).toThrow()
+  expect(() => directive({}, {})).not.toThrow()
 })
