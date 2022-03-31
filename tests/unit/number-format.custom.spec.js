2d1cafa559ef56a51d3beebe13c08003256cd62d
Dipak Sarkar <sdepokbd@gmail.com>
2022-03-31 20:58:40 +0530

    updated is null methods

--- tests/unit/number-format.custom.spec.js
+++ tests/unit/number-format.custom.spec.js
@@ -1,117 +1,130 @@
-import NumberFormat from '../../src/number-format'
+import NumberFormat from "../../src/number-format";
 
-describe('should not throw error on empty config', () => {
-  expect(() => new NumberFormat({
-    prefix: '$',
-    separator: '.',
-    decimal: ',',
+describe("should not throw error on empty config", () => {
+  expect(
+    () =>
+      new NumberFormat({
+        prefix: "$",
+        separator: ".",
+        decimal: ",",
+        null_value: 0,
+      })
+  ).not.toThrow();
+});
+describe("when the value is invalid with custom config", () => {
+  const numberFormat = new NumberFormat({
+    prefix: "$",
+    separator: ".",
+    decimal: ",",
     null_value: 0,
-  })).not.toThrow()
-})
-describe('when the value is invalid with custom config', () => {
+  });
+  it("should return as follows", () => {
+    expect(numberFormat.format("")).toEqual("");
+    expect(numberFormat.format("foo")).toEqual("$0");
+    expect(numberFormat.format("-foo")).toEqual("$0");
+    expect(numberFormat.format("-fo,o-")).toEqual("$0,");
+    expect(numberFormat.format("-fo.o-")).toEqual("$0");
+    expect(numberFormat.format("!@#$%^&*()")).toEqual("$0");
+  });
+  it("should return as follows", () => {
+    expect(numberFormat.clean(true).format("")).toEqual("");
+    expect(numberFormat.clean(true).format("foo")).toEqual("$0");
+    expect(numberFormat.clean(true).format("-foo")).toEqual("$0");
+    expect(numberFormat.clean(true).format("-fo.o-")).toEqual("$0");
+    expect(numberFormat.clean(true).format("-fo,o-")).toEqual("$0");
+    expect(numberFormat.clean(true).format("!@#$%^&*()")).toEqual("$0");
+  });
+  it("should return as follows", () => {
+    expect(numberFormat.clean(true).unformat("")).toEqual("");
+    expect(numberFormat.clean(true).unformat("foo")).toEqual("0");
+    expect(numberFormat.clean(true).unformat("-foo")).toEqual("0");
+    expect(numberFormat.clean(true).unformat("-fo.o-")).toEqual("0");
+    expect(numberFormat.clean(true).unformat("!@#$%^&*()")).toEqual("0");
+  });
+});
+describe("format when options are custom", () => {
   const numberFormat = new NumberFormat({
-    prefix: '$',
-    separator: '.',
-    decimal: ',',
-    null_value: 0,
-  })
-  it('should return as follows', () => {
-    expect(numberFormat.format('')).toEqual('')
-    expect(numberFormat.format('foo')).toEqual('')
-    expect(numberFormat.format('-foo')).toEqual('')
-    expect(numberFormat.format('-fo,o-')).toEqual('')
-    expect(numberFormat.format('-fo.o-')).toEqual('')
-    expect(numberFormat.format('!@#$%^&*()')).toEqual('')
-  })
-  it('should return as follows', () => {
-    expect(numberFormat.clean(true).format('')).toEqual('')
-    expect(numberFormat.clean(true).format('foo')).toEqual('')
-    expect(numberFormat.clean(true).format('-foo')).toEqual('')
-    expect(numberFormat.clean(true).format('-fo.o-')).toEqual('')
-    expect(numberFormat.clean(true).format('-fo,o-')).toEqual('')
-    expect(numberFormat.clean(true).format('!@#$%^&*()')).toEqual('')
-  })
-  it('should return as follows', () => {
-    expect(numberFormat.clean(true).unformat('')).toEqual('')
-    expect(numberFormat.clean(true).unformat('foo')).toEqual('')
-    expect(numberFormat.clean(true).unformat('-foo')).toEqual('')
-    expect(numberFormat.clean(true).unformat('-fo.o-')).toEqual('')
-    expect(numberFormat.clean(true).unformat('!@#$%^&*()')).toEqual('')
-  })
-})
-describe('format when options are custom', () => {
+    prefix: "$",
+    separator: ".",
+    decimal: ",",
+    null_value: "",
+  });
+  it("format string value", () => {
+    expect(numberFormat.format("0")).toEqual("$0");
+    expect(numberFormat.format("0,")).toEqual("$0,");
+    expect(numberFormat.format("-0,0")).toEqual("$0,0");
+    expect(numberFormat.format("0,10")).toEqual("$0,10");
+    expect(numberFormat.format("0,0-")).toEqual("$0,0");
+    expect(numberFormat.format("0,10-")).toEqual("-$0,10");
+    expect(numberFormat.format("12.345,54921")).toEqual("$12.345,54921");
+    expect(numberFormat.format("--12.345,12345")).toEqual("-$12.345,12345");
+    expect(numberFormat.format("12.345.54321,12945")).toEqual(
+      "$1.234.554.321,12945"
+    );
+    expect(numberFormat.format("-12.345,,54321-")).toEqual("-$12.345,54321");
+  });
+  it("format numerical value", () => {
+    expect(numberFormat.format(0)).toEqual("$0");
+    expect(numberFormat.format(0)).toEqual("$0");
+    expect(numberFormat.format(0.0)).toEqual("$0");
+    expect(numberFormat.format(-0.1)).toEqual("-$0,1");
+    expect(numberFormat.format(-0.0)).toEqual("$0");
+    expect(numberFormat.format(0.1)).toEqual("$0,1");
+    expect(numberFormat.format(12345.54921)).toEqual("$12.345,54921");
+    expect(numberFormat.format(12345.12345)).toEqual("$12.345,12345");
+    expect(numberFormat.format(12345.54321)).toEqual("$12.345,54321");
+    expect(numberFormat.format(12345.54321)).toEqual("$12.345,54321");
+  });
+  it("format and clean numerical value", () => {
+    expect(numberFormat.clean(true).format(0)).toEqual("$0");
+    expect(numberFormat.clean(true).format(0)).toEqual("$0");
+    expect(numberFormat.clean(true).format(0.0)).toEqual("$0");
+    expect(numberFormat.clean(true).format(0.1)).toEqual("$0,1");
+    expect(numberFormat.clean(true).format(-0.0)).toEqual("$0");
+    expect(numberFormat.clean(true).format(-0.1)).toEqual("-$0,1");
+    expect(numberFormat.clean(true).format(12345.54921)).toEqual("$12.345,55");
+    expect(numberFormat.clean(true).format(12345.12345)).toEqual("$12.345,12");
+    expect(numberFormat.clean(true).format(12345.54321)).toEqual("$12.345,54");
+    expect(numberFormat.clean(true).format(12345.54321)).toEqual("$12.345,54");
+  });
+});
+describe("unformat when options are default", () => {
   const numberFormat = new NumberFormat({
-    prefix: '$',
-    separator: '.',
-    decimal: ',',
-    null_value: '',
-  })
-  it('format string value', () => {
-    expect(numberFormat.format('0')).toEqual('$0')
-    expect(numberFormat.format('0,')).toEqual('$0,')
-    expect(numberFormat.format('-0,0')).toEqual('$0,0')
-    expect(numberFormat.format('0,10')).toEqual('$0,10')
-    expect(numberFormat.format('0,0-')).toEqual('$0,0')
-    expect(numberFormat.format('0,10-')).toEqual('-$0,10')
-    expect(numberFormat.format('12.345,54921')).toEqual('$12.345,54921')
-    expect(numberFormat.format('--12.345,12345')).toEqual('-$12.345,12345')
-    expect(numberFormat.format('12.345.54321,12945')).toEqual('$1.234.554.321,12945')
-    expect(numberFormat.format('-12.345,,54321-')).toEqual('-$12.345,54321')
-  })
-  it('format numerical value', () => {
-    expect(numberFormat.format(0)).toEqual('')
-    expect(numberFormat.format(0.)).toEqual('')
-    expect(numberFormat.format(0.0)).toEqual('')
-    expect(numberFormat.format(-0.10)).toEqual('-$0,1')
-    expect(numberFormat.format(-0.0)).toEqual('')
-    expect(numberFormat.format(0.10)).toEqual('$0,1')
-    expect(numberFormat.format(12345.54921)).toEqual('$12.345,54921')
-    expect(numberFormat.format(12345.12345)).toEqual('$12.345,12345')
-    expect(numberFormat.format(12345.54321)).toEqual('$12.345,54321')
-    expect(numberFormat.format(12345.54321)).toEqual('$12.345,54321')
-  })
-  it('format and clean numerical value', () => {
-    expect(numberFormat.clean(true).format(0)).toEqual('')
-    expect(numberFormat.clean(true).format(0.)).toEqual('')
-    expect(numberFormat.clean(true).format(0.0)).toEqual('')
-    expect(numberFormat.clean(true).format(0.10)).toEqual('$0,1')
-    expect(numberFormat.clean(true).format(-0.0)).toEqual('')
-    expect(numberFormat.clean(true).format(-0.10)).toEqual('-$0,1')
-    expect(numberFormat.clean(true).format(12345.54921)).toEqual('$12.345,55')
-    expect(numberFormat.clean(true).format(12345.12345)).toEqual('$12.345,12')
-    expect(numberFormat.clean(true).format(12345.54321)).toEqual('$12.345,54')
-    expect(numberFormat.clean(true).format(12345.54321)).toEqual('$12.345,54')
-  })
-})
-describe('unformat when options are default', () => {
-  const numberFormat = new NumberFormat({
-    prefix: '$',
-    separator: '.',
-    decimal: ',',
-    null_value: '',
-  })
-  it('unformat string value', () => {
-    expect(numberFormat.clean(true).unformat('0')).toEqual('0')
-    expect(numberFormat.clean(true).unformat('0,')).toEqual('0')
-    expect(numberFormat.clean(true).unformat('-0,0')).toEqual('0')
-    expect(numberFormat.clean(true).unformat('0,10')).toEqual('0.1')
-    expect(numberFormat.clean(true).unformat('0,0-')).toEqual('0')
-    expect(numberFormat.clean(true).unformat('0,10-')).toEqual('-0.1')
-    expect(numberFormat.clean(true).unformat('12.345,54921')).toEqual('12345.55')
-    expect(numberFormat.clean(true).unformat('--12.345,12345')).toEqual('-12345.12')
-    expect(numberFormat.clean(true).unformat('12.345.54321,12345')).toEqual('1234554321.12')
-    expect(numberFormat.clean(true).unformat('-12.345,,54321-')).toEqual('-12345.54')
-  })
-  it('unformat numerical value', () => {
-    expect(numberFormat.clean(true).unformat(0)).toEqual('')
-    expect(numberFormat.clean(true).unformat(0.)).toEqual('')
-    expect(numberFormat.clean(true).unformat(0.0)).toEqual('')
-    expect(numberFormat.clean(true).unformat(-0.10)).toEqual('-0.1')
-    expect(numberFormat.clean(true).unformat(-0.0)).toEqual('')
-    expect(numberFormat.clean(true).unformat(0.10)).toEqual('0.1')
-    expect(numberFormat.clean(true).unformat(12345.54921)).toEqual('12345.55')
-    expect(numberFormat.clean(true).unformat(12345.12345)).toEqual('12345.12')
-    expect(numberFormat.clean(true).unformat(12345.54321)).toEqual('12345.54')
-    expect(numberFormat.clean(true).unformat(12345.54321)).toEqual('12345.54')
-  })
-})
+    prefix: "$",
+    separator: ".",
+    decimal: ",",
+    null_value: "",
+  });
+  it("unformat string value", () => {
+    expect(numberFormat.clean(true).unformat("0")).toEqual("0");
+    expect(numberFormat.clean(true).unformat("0,")).toEqual("0");
+    expect(numberFormat.clean(true).unformat("-0,0")).toEqual("0");
+    expect(numberFormat.clean(true).unformat("0,10")).toEqual("0.1");
+    expect(numberFormat.clean(true).unformat("0,0-")).toEqual("0");
+    expect(numberFormat.clean(true).unformat("0,10-")).toEqual("-0.1");
+    expect(numberFormat.clean(true).unformat("12.345,54921")).toEqual(
+      "12345.55"
+    );
+    expect(numberFormat.clean(true).unformat("--12.345,12345")).toEqual(
+      "-12345.12"
+    );
+    expect(numberFormat.clean(true).unformat("12.345.54321,12345")).toEqual(
+      "1234554321.12"
+    );
+    expect(numberFormat.clean(true).unformat("-12.345,,54321-")).toEqual(
+      "-12345.54"
+    );
+  });
+  it("unformat numerical value", () => {
+    expect(numberFormat.clean(true).unformat(0)).toEqual("0");
+    expect(numberFormat.clean(true).unformat(0)).toEqual("0");
+    expect(numberFormat.clean(true).unformat(0.0)).toEqual("0");
+    expect(numberFormat.clean(true).unformat(-0.1)).toEqual("-0.1");
+    expect(numberFormat.clean(true).unformat(-0.0)).toEqual("0");
+    expect(numberFormat.clean(true).unformat(0.1)).toEqual("0.1");
+    expect(numberFormat.clean(true).unformat(12345.54921)).toEqual("12345.55");
+    expect(numberFormat.clean(true).unformat(12345.12345)).toEqual("12345.12");
+    expect(numberFormat.clean(true).unformat(12345.54321)).toEqual("12345.54");
+    expect(numberFormat.clean(true).unformat(12345.54321)).toEqual("12345.54");
+  });
+});
--- tests/unit/number-format.reverse-fill.spec.js
+++ tests/unit/number-format.reverse-fill.spec.js
@@ -1,21 +1,20 @@
-import NumberFormat from '../../src/number-format'
+import NumberFormat from "../../src/number-format";
 
-describe('when enabled reverse fill', () => {
+describe("when enabled reverse fill", () => {
   const numberFormat = new NumberFormat({
-    reverseFill: true
-  })
-  it('should return as follows', () => {
-    expect(numberFormat.format('sdfgasd55468.546')).toEqual('554,685.46')
-    expect(numberFormat.format('sdfgasd55468.546')).toEqual('554,685.46')
-    expect(numberFormat.format('sdfgasd55468.546-')).toEqual('-554,685.46')
-    expect(numberFormat.format('-1234.6512')).toEqual('-123,465.12')
-  })
-  it('should return as follows', () => {
-    expect(numberFormat.unformat('sdfgasd55468.546')).toEqual('554685.46')
-    expect(numberFormat.unformat('sdfgasd55468.546')).toEqual('554685.46')
-    expect(numberFormat.unformat('sdfgasd55468.546-')).toEqual('-554685.46')
-    expect(numberFormat.unformat('-1234.6512')).toEqual('-123465.12')
-  })
-})
-
-
+    reverseFill: true,
+  });
+  it("should return as follows", () => {
+    expect(numberFormat.format("sdfgasd55468.546")).toEqual("554,685.46");
+    expect(numberFormat.format("sdfgasd55468.546")).toEqual("554,685.46");
+    expect(numberFormat.format("sdfgasd55468.546-")).toEqual("-554,685.46");
+    expect(numberFormat.format("-1234.6512")).toEqual("-123,465.12");
+    expect(numberFormat.format("0")).toEqual("0.00");
+  });
+  it("should return as follows", () => {
+    expect(numberFormat.unformat("sdfgasd55468.546")).toEqual("554685.46");
+    expect(numberFormat.unformat("sdfgasd55468.546")).toEqual("554685.46");
+    expect(numberFormat.unformat("sdfgasd55468.546-")).toEqual("-554685.46");
+    expect(numberFormat.unformat("-1234.6512")).toEqual("-123465.12");
+  });
+});
