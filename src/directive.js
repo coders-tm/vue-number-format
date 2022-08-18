3b9df2115134c1bc8d5124929868d9c6d51272ea
Dipak Sarkar <hello@dipaksarkar.in>
2022-08-18 17:06:01 +0530

    fix the issue of no update on decimal key down

--- src/directive.js
+++ src/directive.js
@@ -42,7 +42,7 @@ export default {
       ) {
         e.preventDefault()
         el.setRangeText(config.decimal)
-        el.dispatchEvent(new Event('input'))
+        core.updateValue(el, null, { emit: true }, e)
         core.updateCursor(el, el.value.indexOf(config.decimal) + 1)
       } else if (
         ([110, 190].includes(e.keyCode) || e.key === config.decimal) &&
