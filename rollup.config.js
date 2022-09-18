3849f0575d8988ff130748eac1af91d90cdfefc1
Dipak Sarkar <hello@dipaksarkar.in>
2022-09-18 13:05:31 +0530

    updated project

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,28 @@
-import vue from "rollup-plugin-vue";
-import filesize from "rollup-plugin-filesize";
-import pkg from "./package.json";
+import vue from 'rollup-plugin-vue'
+import filesize from 'rollup-plugin-filesize'
+import pkg from './package.json'
 
 const banner = `/**
- * Vue Number Input ${pkg.version}
+ * Vue Number Format ${pkg.version}
  * (c) 2021-${new Date().getFullYear()} ${pkg.author}
  * @license ${pkg.license}
- */`;
+ */`
 
 export default {
-  input: "src/index.js",
+  input: 'src/index.js',
   output: [
     {
       file: pkg.main,
-      format: "cjs",
-      exports: "named",
+      format: 'cjs',
+      exports: 'named',
       banner,
     },
     {
       file: pkg.module,
-      format: "es",
-      exports: "named",
+      format: 'es',
+      exports: 'named',
       banner,
     },
   ],
   plugins: [vue(), filesize()],
-};
+}
