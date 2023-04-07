6ddb4e071c1e725318289f01a12f8ae203318122
Dipak Sarkar <hello@dipaksarkar.in>
2023-04-07 21:07:55 +0530

    Revert "[Fix] Mutation issue"
    This reverts commit 28d45e9eca2972a30a0fe88b0fdf7190346fbcb6.

--- src/core.ts
+++ src/core.ts
@@ -161,6 +161,7 @@ export function inputHandler(event: CustomInputEvent) {
   updateCursor(target, positionFromEnd)
 
   if (oldValue !== target.value) {
+    // target.oldValue = masked
     target.dispatchEvent(InputEvent('input'))
   }
 }
