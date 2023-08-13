9c33578ca2f44e238dd57181126897da12649c0f
Dipak Sarkar <hello@dipaksarkar.in>
2023-08-13 12:21:59 +0530

    docs updated

--- docs/.vuepress/components/BaseInput.vue
+++ docs/.vuepress/components/BaseInput.vue
@@ -3,7 +3,7 @@
     :value="modelValue"
     :type="type"
     class="shadow-sm rounded-md text-base transition-all disabled:cursor-not-allowed disabled:border-gray-300 disabled:text-gray-300 focus:border-primary focus:ring focus:ring-offset-0 focus:ring-primary focus:ring-opacity-50"
-    @change="$emit('update:modelValue', $event.target.value)"
+    @input="$emit('update:modelValue', $event.target.value)"
   />
 </template>
 
