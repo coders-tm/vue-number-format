0f6f4b4a3e8f67fef4ff4590624a7b0673031af3
Dipak Sarkar <hello@dipaksarkar.in>
2024-12-12 15:41:26 +0530

    [Fix] Base Select

--- docs/.vuepress/components/BaseSelect.vue
+++ docs/.vuepress/components/BaseSelect.vue
@@ -1,7 +1,6 @@
 <template>
   <select
     :value="modelValue"
-    type="text"
     class="shadow-sm rounded-md text-base transition-all disabled:cursor-not-allowed disabled:border-gray-300 disabled:text-gray-300 focus:border-primary focus:ring focus:ring-offset-0 focus:ring-primary focus:ring-opacity-50ß"
     @input="$emit('update:modelValue', $event.target.value)"
   >
@@ -19,7 +18,7 @@
 
 <script>
 export default {
-  name: 'BaseInput',
+  name: 'BaseSelect',
   props: {
     modelValue: {
       default: undefined
